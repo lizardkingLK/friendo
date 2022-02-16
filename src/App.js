ef25fbcbba039cc1afa4fead0e288403954ef685
lizardkingLK <chansanfdo@gmail.com>
2022-02-16 13:44:12 +0530

    finallized with styling and about

--- src/App.js
+++ src/App.js
@@ -23,9 +23,14 @@ function App() {
     const GetImage = (e) => {
         if (text !== "") {
             let target = targetRef.current;
+            let targetWrap = document.createElement("section");
+            let targetTemp = target.cloneNode(true);
+            targetWrap.setAttribute("class", "largeDisplayText");
+            targetWrap.append(targetTemp);
+            document.body.appendChild(targetWrap);
             let filename = GetId(36);
             let options = { backgroundColor: BackgroundColor };
-            html2canvas(target, options)
+            html2canvas(targetWrap, options)
                 .then((canvas) => {
                     let lnk = document.createElement("a");
                     lnk.download = filename;
@@ -33,6 +38,7 @@ function App() {
                     e = new MouseEvent("click");
                     lnk.dispatchEvent(e);
                 });
+            targetWrap.remove();
         }
     }
 
@@ -40,19 +46,21 @@ function App() {
         <div className="app">
             <header className="appHeader">
                 <nav className='nav'>
-                    <div className='brandNavbar'>
+                    <div className='brandNavbar' title='Call It. friendo •'>
                         <h3 className='brandLogo'>•</h3>
                         <h3 className='brandName'>friendo</h3>
                     </div>
                     <ul className="listNavbar">
                         <li className="listNavbarItem">
-                            <h5 className='controlInputMeta'>
-                                {`${CharLimit - text.length} characters remaining`}
-                            </h5>
+                            <h3 className='controlInputMeta listNavbarItemAnchorText'
+                                title={`${CharLimit - text.length} Characters Remaining`}>
+                                {CharLimit - text.length}
+                            </h3>
                         </li>
                         <li className="listNavbarItem">
                             <button onClick={(e) => ClearInput()}
-                                className='listNavbarItemAnchor primaryBtn'>
+                                className='listNavbarItemAnchor primaryBtn'
+                                title='Clear Entered Text'>
                                 <h3 className='listNavbarItemAnchorText'>
                                     <i className="fa-solid fa-eraser"></i>
                                 </h3>
@@ -60,7 +68,8 @@ function App() {
                         </li>
                         <li className="listNavbarItem">
                             <button onClick={(e) => GetImage(e)}
-                                className='listNavbarItemAnchor primaryBtn'>
+                                className='listNavbarItemAnchor primaryBtn'
+                                title='Download as Image'>
                                 <h3 className='listNavbarItemAnchorText'>
                                     <i className="fas fa-download"></i>
                                 </h3>
@@ -68,7 +77,8 @@ function App() {
                         </li>
                         <li className="listNavbarItem">
                             <button onClick={(e) => setNAbout(nAbout + 1)}
-                                className='listNavbarItemAnchor primaryBtn'>
+                                className='listNavbarItemAnchor primaryBtn'
+                                title='About Us'>
                                 <h3 className='listNavbarItemAnchorText'>
                                     <i className="fa fa-info-circle"></i>
                                 </h3>
@@ -82,10 +92,11 @@ function App() {
                 <div className='appBodyControl'>
                     <textarea name='controlInput' className='controlInput primaryTextarea'
                         id='controlInput' placeholder='Enter your text...' ref={inputRef}
-                        onChange={(e) => SetInput(e)} onPaste={(e) => SetInput(e)}
-                        maxLength={CharLimit}></textarea>
+                        onChange={(e) => SetInput(e)} onPaste={(e) => SetInput(e)} maxLength={CharLimit}
+                        title={`${CharLimit - text.length} Characters Remaining`}></textarea>
                 </div>
-                <h1 ref={targetRef} className='appBodyDisplay appBodyDisplayTitle'>{text}</h1>
+                <h1 className='appBodyDisplay appBodyDisplayTitle'
+                    ref={targetRef} title='Preview of the Output'>{text}</h1>
             </section>
 
             <section ref={aboutRef} className="appAbout">
@@ -95,9 +106,11 @@ function App() {
                         <i className="fa fa-close"></i>
                     </h3>
                 </button>
-                <h3 className="aboutCreatedBy">Created by Sandeep Fernando for <b>AMUS</b>&trade;</h3>
-                <blockquote className='aboutQuote' cite="http://www.worldwildlife.org/who/index.html">
-                    "What's the most you ever lost on a coin toss ?"
+                <h3 className="aboutCreatedBy" title='https://github.com/AMUSTechCo'>
+                    Created by Sandeep Fernando for <b>AMUS</b>&trade;</h3>
+                <blockquote className='aboutQuote' cite="https://www.imdb.com/title/tt0477348/"
+                    title='https://www.imdb.com/title/tt0477348/'>
+                    "What's the most you ever lost on a coin toss?"
                 </blockquote>
             </section>
         </div>
